17d613b1c4d43b49dcaf03080d9b339ea15385e4
Vuong Nguyen <vuongnguyen@chidoanh.com>
2020-08-26 13:45:08 +0700

    update unit test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import { inCrement, deCrement } from "../src/action/toDoApp.action";
 
-export const App = ({ counter }) => {
-  console.log(counter);
-  return <p>{counter}</p>;
+export const App = (props) => {
+  const { counter, increment, decrement } = props;
+  return (
+    <div>
+      <p>{counter}</p>
+      <button id="increment" type="button" onClick={() => increment()}>
+        Increment
+      </button>
+      <button id="decrement" type="button" onClick={() => decrement()}>
+        Decrement
+      </button>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => ({
   counter: state.toDoAppp.counter,
 });
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      increment: inCrement,
+      decrement: deCrement,
+    },
+    dispatch
+  );
+};
 
-export default connect(mapStateToProps)(App);
+const connected = connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default connected;
--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,27 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import { shallow } from "enzyme";
 
 // Notice the non default export here
-import App from "./App";
-import Adapter from "enzyme-adapter-react-16";
-Enzyme.configure({ adapter: new Adapter() });
+import { App } from "./App";
 
 let component;
 
 const mockProps = {
-  counter: 0,
+  counter: 1,
+  increment: jest.fn(() => 1),
+  decrement: jest.fn(() => -1),
 };
 
 describe("Counter Component", () => {
-  component = shallow(<App {...mockProps} />);
+  beforeAll(() => {
+    component = shallow(<App {...mockProps} />);
+  });
 
   it("displays the counter value", () => {
     expect(component.find("p").text()).toBe("0");
+  });
+  it("triggers the increment function", () => {
+    component.find("#increment").simulate("click");
+    expect(mockProps.increment.mock.results[0].value).toBe(1);
   });
 });
